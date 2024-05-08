bfbd80846938a9f673eaa47ff1273e3b3b147222
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2024-05-08 20:48:31 -0300

    Adicionando primeiro estudo

---
+++ EstudoJava/src/variaveis/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class TiposVariaveis {
+    public static void main(String [] args){
+        int idade = 17; // Tipo Inteiro
+        double salario = 1502.0; // Tipo Double (Armazena mais números decimais 10 casas)
+        float pontuacao = 11.24f; // Tipo Float (Armazena menos números decimais 6 casas)
+        String nome = "Wladison"; // Tipo String (Armazena palavras)
+        char letra = 'W'; // Tipo Char (Parecido com String mas armazena somente uma letra)
+        boolean escolha = true; // Tipo Booleano (Serve para escolhas True ou False)
+
+        final int maiorIdade = 18; // Tipo Int mas Const (Variável que não pode ter seu valor alterado)
+    }
+}
